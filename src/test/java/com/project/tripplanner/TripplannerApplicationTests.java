4322d1ff09f8bae327b39eb991df5b046cbbd576
Fedorca <steli.fedorca25@gmail.com>
2021-03-12 23:21:07 +0200

    first commit

---
+++ src/test/java/com/project/tripplanner/TripplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tripplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
