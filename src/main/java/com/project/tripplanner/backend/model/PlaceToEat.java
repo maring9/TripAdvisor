e92d089c152dd2756c778392954bf01c4a01d7ad
Fedorca <steli.fedorca25@gmail.com>
2021-03-21 17:04:41 +0200

    I have added title,description and id fields for the "PlaceToEat" class

--- src/main/java/com/project/tripplanner/backend/model/PlaceToEat.java
+++ src/main/java/com/project/tripplanner/backend/model/PlaceToEat.java
@@ -1,5 +1,37 @@
 package com.project.tripplanner.backend.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "table_places_to_eat")
 public class PlaceToEat extends PlaceToVisit {
 
+    public PlaceToEat(String title, String description) {
+        super(title, description);
+        this.title = title;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id ;
+
+    @Column(name = "title")
+    private String title ;
+
+    @Column(name = "description")
+    private String description ;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
