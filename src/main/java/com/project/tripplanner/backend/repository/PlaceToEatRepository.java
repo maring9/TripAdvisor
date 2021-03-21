c89b7650fdaebf4871368a8e6d7ae30c73046ecc
Fedorca <steli.fedorca25@gmail.com>
2021-03-21 17:23:53 +0200

    I have added another two repository interfaces for "PlaceToEat" and "PlaceToStay" classes in order to perform all crud operations

---
+++ src/main/java/com/project/tripplanner/backend/repository/PlaceToEatRepository.java
@@ -0,0 +1,7 @@
+package com.project.tripplanner.backend.repository;
+
+import com.project.tripplanner.backend.model.PlaceToEat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceToEatRepository extends JpaRepository<PlaceToEat,Long> {
+}
---
+++ src/main/java/com/project/tripplanner/backend/repository/PlaceToStayRepository.java
@@ -0,0 +1,7 @@
+package com.project.tripplanner.backend.repository;
+
+import com.project.tripplanner.backend.model.PlaceToStay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceToStayRepository extends JpaRepository<PlaceToStay,Long> {
+}
