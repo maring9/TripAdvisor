ff092ce365a70d31a2265be6333b1e700149b62b
Fedorca <stelian.fedorca25@gmail.com>
2021-03-22 14:47:00 +0200

    I have added a method so that we can add new places

--- src/main/java/com/project/tripplanner/backend/controller/PlaceToStayController.java
+++ src/main/java/com/project/tripplanner/backend/controller/PlaceToStayController.java
@@ -7,10 +7,7 @@ import com.project.tripplanner.backend.repository.PlaceToVisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,5 +35,16 @@ public class PlaceToStayController {
         }catch (Exception e){
             return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping("/places_to_stay")
+    public ResponseEntity<PlaceToStay> createPlace(@RequestBody PlaceToStay place){
+        try{
+            PlaceToStay placeToStay = placeToStayRepository.save(new PlaceToStay(place.getTitle(),place.getDescription()));
+            return new ResponseEntity<>(placeToStay,HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
