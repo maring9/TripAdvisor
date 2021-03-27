b3ced572b9f126e825977809d1975f94ba31d255
Fedorca <stelian.fedorca25@gmail.com>
2021-03-28 00:19:41 +0200

    I have created "UserPrincipal" class that is used to store user info which is later encapsulated into an Authentication object in order to be sent back to the client

---
+++ src/main/java/com/project/tripplanner/backend/model/UserPrincipal.java
@@ -0,0 +1,66 @@
+package com.project.tripplanner.backend.model;
+
+
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+//store user information which is later encapsulated into Authentication objects.
+public class UserPrincipal implements UserDetails {
+
+    private String username ;
+
+    private String email ;
+
+    @JsonIgnore
+    private String password;
+
+    private List<GrantedAuthority> grantedAuthorityList;
+
+    public UserPrincipal(){}
+
+    public UserPrincipal(User user) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
