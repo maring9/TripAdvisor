087c5f5842bd6b8642028049aa5b0a6e4547d53d
Fedorca <stelian.fedorca25@gmail.com>
2021-03-27 23:28:07 +0200

    I have updated the "User" class with almost all the information fields we need to store  I have created the "Role" class  I have created "RoleName" enum class

---
+++ src/main/java/com/project/tripplanner/backend/model/RoleName.java
@@ -0,0 +1,5 @@
+package com.project.tripplanner.backend.model;
+
+public enum RoleName {
+    ROLE_USER
+}
--- src/main/java/com/project/tripplanner/backend/model/User.java
+++ src/main/java/com/project/tripplanner/backend/model/User.java
@@ -1,9 +1,77 @@
 package com.project.tripplanner.backend.model;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users",uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
 public class User {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+
+    @NotBlank
+    private String username ;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){}
+
+    public User(@NotBlank String username, @NotBlank @Email String email, @NotBlank String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
