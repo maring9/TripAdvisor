a0c90e883d04f4598728770668b5540b2ad13902
Fedorca <stelian.fedorca25@gmail.com>
2021-03-28 00:29:02 +0200

    I have added the information fields required for a user

--- src/main/java/com/project/tripplanner/backend/model/UserPrincipal.java
+++ src/main/java/com/project/tripplanner/backend/model/UserPrincipal.java
@@ -3,18 +3,22 @@ package com.project.tripplanner.backend.model;
 
 import net.minidev.json.annotate.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //store user information which is later encapsulated into Authentication objects.
 public class UserPrincipal implements UserDetails {
 
-    private String username ;
+    private Long id;
 
-    private String email ;
+    private String username;
+
+    private String email;
 
     @JsonIgnore
     private String password;
@@ -24,24 +28,36 @@ public class UserPrincipal implements UserDetails {
     public UserPrincipal(){}
 
     public UserPrincipal(User user) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        grantedAuthorityList = user.getRoles().stream().map(role ->
+                new SimpleGrantedAuthority(role.getName().name())
+        ).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return grantedAuthorityList;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
@@ -51,16 +67,16 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
