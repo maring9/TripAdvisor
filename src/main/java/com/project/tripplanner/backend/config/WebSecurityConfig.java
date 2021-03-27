9a98d21109030c2f80c3b36ee7b9aacf0a538067
Fedorca <stelian.fedorca25@gmail.com>
2021-03-28 00:48:44 +0200

    I have added a a reference to UserDetailsServiceImpl and a TODO comment

--- src/main/java/com/project/tripplanner/backend/config/WebSecurityConfig.java
+++ src/main/java/com/project/tripplanner/backend/config/WebSecurityConfig.java
@@ -1,17 +1,30 @@
 package com.project.tripplanner.backend.config;
 
+import com.project.tripplanner.backend.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
         prePostEnabled = true
 )
+
+// A.K.A Jwt Token Authentication Spring Security
+//or here we use Jwt Token Authentication Spring Security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+   //TODO : continue from here with JwtAuthEntryPoint
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
