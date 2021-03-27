045e4df399e619035478ab36ae5d4dee90a7dc63
Fedorca <stelian.fedorca25@gmail.com>
2021-03-28 00:08:59 +0200

    I have created "WebSecurityConfig" class

---
+++ src/main/java/com/project/tripplanner/backend/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.project.tripplanner.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+}
