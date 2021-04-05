265649f2362b18e004f8208252261773d4605d2d
Fedorca <stelian.fedorca25@gmail.com>
2021-04-05 13:35:46 +0300

    I've changed width and height of the image profile in login and register component

--- src/main/java/com/project/tripplanner/backend/controller/TestRestAPIs.java
+++ src/main/java/com/project/tripplanner/backend/controller/TestRestAPIs.java
@@ -1,9 +1,11 @@
 package com.project.tripplanner.backend.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class TestRestAPIs {
 
--- src/main/java/com/project/tripplanner/frontend/angularwork/src/app/app-routing.module.ts
+++ src/main/java/com/project/tripplanner/frontend/angularwork/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {RegisterComponent} from "./register/register.component";
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import {BoardUserComponent} from "./board-user/board-user.component";
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
-  {path: 'register', component:RegisterComponent}
+  {path: 'register', component:RegisterComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
