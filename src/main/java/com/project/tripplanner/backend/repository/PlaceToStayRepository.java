dcdaef0df31a24a6d9c182094d8500ac9ff44eaf
Stelian <stelian.fedorca25@gmail.com>
2021-05-10 17:07:07 +0300

    i've added rating field for both PlaceToEat and PlaceToStay entity

--- src/main/java/com/project/tripplanner/backend/repository/PlaceToStayRepository.java
+++ src/main/java/com/project/tripplanner/backend/repository/PlaceToStayRepository.java
@@ -3,10 +3,12 @@ package com.project.tripplanner.backend.repository;
 import com.project.tripplanner.backend.model.PlaceToStay;
 import com.project.tripplanner.backend.model.PlaceToVisit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlaceToStayRepository extends JpaRepository<PlaceToStay,Long> {
-    List<PlaceToStay> findPlacesByCityName(String cityName);
+    List<PlaceToStay> findByCityName(String cityName);
 
 }
