bc051eec61174bcd82a5595f0aedcbe29b46bf91
Stelian <stelian.fedorca25@gmail.com>
2021-05-13 15:00:35 +0300

    i've added setter/getters and constructor for Bookmark class

--- src/main/java/com/project/tripplanner/backend/model/Bookmark.java
+++ src/main/java/com/project/tripplanner/backend/model/Bookmark.java
@@ -1,6 +1,7 @@
 package com.project.tripplanner.backend.model;
 
 import javax.persistence.*;
+import java.awt.print.Book;
 
 @Entity
 @Table(name = "bookmarks")
@@ -10,10 +11,33 @@ public class Bookmark {
     @Column(name = "id")
     private long id;
 
+    @Column(name = "locationName")
     private String locationName;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id",referencedColumnName = "id")
     private User userId;
 
+    public Bookmark(){}
+
+    public Bookmark(String locationName, User userId) {
+        this.locationName = locationName;
+        this.userId = userId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
 }
